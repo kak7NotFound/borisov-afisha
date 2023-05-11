9ffbff3b71a26826919d56bcbd2c6bb65fc2b99b
kak7 <38856979+kak7NotFound@users.noreply.github.com>
2023-05-11 16:17:42 +0300

    a

--- borisovAfisha/app/src/main/java/com/example/borisovafisha/MainActivity.kt
+++ borisovAfisha/app/src/main/java/com/example/borisovafisha/MainActivity.kt
@@ -2,10 +2,6 @@ package com.example.borisovafisha
 
 import android.os.Bundle
 import androidx.activity.ComponentActivity
-import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
@@ -17,9 +13,42 @@ import com.example.borisovafisha.ui.theme.BorisovAfishaTheme
 class MainActivity : ComponentActivity() {
 
     private val movies = listOf(
-        Movie("Interstellar", 8.6, "Christopher Nolan"),
-        Movie("The Godfather", 9.2, "Francis Ford Coppola"),
-        Movie("The Shawshank Redemption", 9.3, "Frank Darabont")
+        Movie(
+            title = "The Shawshank Redemption",
+            description = "Two imprisoned men bond over a number of years, finding solace and eventual redemption through acts of common decency.",
+            poster = "https://example.com/shawshank_redemption.jpg",
+            director = "Frank Darabont",
+            writers = "Stephen King (short story), Frank Darabont (screenplay)",
+            release_date = "October 14, 1994",
+            running_time = "142 minutes",
+            country = "United States",
+            budget = "$25 million",
+            rating = "9.3"
+        ),
+        Movie(
+            title = "Inception",
+            description = "A thief who steals corporate secrets through the use of dream-sharing technology is given the inverse task of planting an idea into the mind of a C.E.O.",
+            poster = "https://example.com/inception.jpg",
+            director = "Christopher Nolan",
+            writers = "Christopher Nolan",
+            release_date = "July 8, 2010",
+            running_time = "148 minutes",
+            country = "United States",
+            budget = "$160 million",
+            rating = "8.8"
+        ),
+        Movie(
+            title = "The Lord of the Rings: The Fellowship of the Ring",
+            description = "A meek Hobbit from the Shire and eight companions set out on a journey to destroy the powerful One Ring and save Middle-earth from the Dark Lord Sauron.",
+            poster = "https://example.com/lotr_fellowship.jpg",
+            director = "Peter Jackson",
+            writers = "J.R.R. Tolkien (novel), Fran Walsh (screenplay)",
+            release_date = "December 19, 2001",
+            running_time = "178 minutes",
+            country = "New Zealand, United States",
+            budget = "$93 million",
+            rating = "8.8"
+        )
     )
 
     override fun onCreate(savedInstanceState: Bundle?) {
--- borisovAfisha/app/src/main/java/com/example/borisovafisha/Movie.kt
+++ borisovAfisha/app/src/main/java/com/example/borisovafisha/Movie.kt
@@ -1,3 +1,14 @@
 package com.example.borisovafisha
 
-class Movie(val title: String, val rating: Double, val director: String)
+data class Movie(
+    val title: String,
+    val description: String,
+    val poster: String,
+    val director: String,
+    val writers: String,
+    val release_date: String,
+    val running_time: String,
+    val country: String,
+    val budget: String,
+    val rating: String
+)
